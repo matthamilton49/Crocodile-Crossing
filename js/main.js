79c8c43ff090b03f00b40e353bbfc57304d00609
Matt Hamilton <matt.hamilton49@gmail.com>
2022-12-02 17:00:15 +0100

    Player created and has movement in 4 directions

--- js/main.js
+++ js/main.js
@@ -1,15 +1,74 @@
 class Zebra {
-    constructor () {
-        this.width = 10;
-        this.height =10;
-        this.positionX = 50;
-        this.positionY = 0;
-        this.movementAmount = 10;
-        this.domElement = null;
-        this.createDomElement();
+  constructor() {
+    this.width = 10;
+    this.height = 10;
 
+    this.positionX = 40;
+    this.positionY = 0;
+
+    this.movementAmount = 10;
+
+    this.crocElement = null;
+    this.createCrocElement();
+  }
+  createCrocElement() {
+    this.crocElement = document.createElement("div"); //step 1: create the DOM element
+
+    this.crocElement.id = "zebra"; //step 2: add content
+
+    this.crocElement.style.width = this.width + "vw"; //player size
+    this.crocElement.style.height = this.height + "vh";
+
+    this.crocElement.style.bottom = this.positionY + "vh"; //player position
+    this.crocElement.style.left = this.positionX + "vw";
+
+    const gameBoardElement = document.getElementById("gameBoard"); //step 3: append to parent
+    gameBoardElement.appendChild(this.crocElement);
+  }
+
+  moveLeft() {
+    if (this.positionX > 0) {
+      this.positionX -= this.movementAmount;
+      console.log("new position x axis...." + this.positionX);
+      this.crocElement.style.left = this.positionX + "vw";
     }
+  }
 
+  moveRight() {
+    if (this.positionX < 100 - this.width) {
+      this.positionX += this.movementAmount;
+      console.log("new position x axis...." + this.positionX);
+      this.crocElement.style.left = this.positionX + "vw";
+    }
+  }
+
+  moveUp() {
+    if (this.positionY < 100 - this.height) {
+      this.positionY += this.movementAmount;
+      console.log("new position y axis...." + this.positionY);
+      this.crocElement.style.bottom = this.positionY + "vh";
+    }
+  }
+
+  moveDown() {
+    if (this.positionY > 0) {
+      this.positionY -= this.movementAmount;
+      console.log("new position y axis...." + this.positionY);
+      this.crocElement.style.bottom = this.positionY + "vh";
+    }
+  }
 }
 
-console.log("hello world")
+const zebra = new Zebra();
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowRight" || event.key === "d") {
+    zebra.moveRight();
+  } else if (event.key === "ArrowLeft" || event.key === "a") {
+    zebra.moveLeft();
+  } else if (event.key === "ArrowUp" || event.key === "w") {
+    zebra.moveUp();
+  } else if (event.key === "ArrowDown" || event.key === "s") {
+    zebra.moveDown();
+  }
+});
