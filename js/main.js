4fe3e97b946b6d8422754b248ccd63eb826bf1e9
Matt Hamilton <matt.hamilton49@gmail.com>
2022-12-02 16:08:36 +0100

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+class Zebra {
+    constructor () {
+        this.width = 10;
+        this.height =10;
+        this.positionX = 50;
+        this.positionY = 0;
+        this.movementAmount = 10;
+        this.domElement = null;
+        this.createDomElement();
+
+    }
+
+}
+
+console.log("hello world")
